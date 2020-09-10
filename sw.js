8e021359e250c2f9f8c5419053de4b0bb998fdc7
wikugw <wikugw@gmail.com>
2020-09-10 14:46:45 +0700

    add cache

--- sw.js
+++ sw.js
@@ -1,12 +1,34 @@
-// install service worker
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
+// install event
 self.addEventListener('install', evt => {
-  console.log('Service worker has been installed');
-})
-// activate service worker
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
 self.addEventListener('activate', evt => {
-  console.log('Service worker has been activated');
-})
-// fetch even
+  //console.log('service worker activated');
+});
+
+// fetch event
 self.addEventListener('fetch', evt => {
-  console.log('Fetch Event', evt);
-})
+  //console.log('fetch event', evt);
+});
